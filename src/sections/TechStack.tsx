6a34f74a06d65275478a54aabba9345527af86cd
Luis Gonzalez <lmigonzalezs@gmail.com>
2024-07-26 12:24:43 -0400

    draggable

--- src/sections/TechStack.tsx
+++ src/sections/TechStack.tsx
@@ -89,7 +89,7 @@ export default function TechStack() {
                   </div>
                 </CarouselItem>
               );
-              F;
+              
             })}
           </CarouselContent>
         </Carousel>
