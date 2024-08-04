2c531f91f11ead23f8f2eb6001a26c9477a8d6d9
lmigonzalez <lmigonzalezs@gmail.com>
2024-08-04 16:59:25 -0400

    live chat

--- src/components/LiveChat.jsx
+++ src/components/LiveChat.jsx
@@ -5,7 +5,7 @@ const LiveChat = () => {
   useEffect(() => {
     const s1 = document.createElement("script");
     s1.async = true;
-    s1.src = "https://embed.tawk.to/66afe3ca32dca6db2cb9ef09/1i4fh3jk0";
+    s1.src = "https://embed.tawk.to/66afeb7f1601a2195ba0b7e3/1i4fivq8j";
     s1.charset = "UTF-8";
     s1.setAttribute("crossorigin", "*");
     document.body.appendChild(s1);
