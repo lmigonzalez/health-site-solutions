b6dd4617f61761965bbab3969c3d653cd0b3bec0
Luis Gonzalez <lmigonzalezs@gmail.com>
2024-08-05 08:29:12 -0400

    new

--- src/app/(pages)/privacy-policy/page.tsx
+++ src/app/(pages)/privacy-policy/page.tsx
@@ -1,5 +1,87 @@
 import React from "react";
 
 export default function Page() {
-  return <main>page</main>;
+  return (
+    <main>
+      <article className="article">
+        <h1>Privacy Policy</h1>
+        <p>Effective Date: [08/05/2024]</p>
+
+        <p>
+          <b>HealthWebExperts.com</b> is committed to protecting your privacy.
+          This Privacy Policy explains how we collect, use, disclose, and
+          safeguard your information when you visit our website or use our
+          services.
+        </p>
+
+        <h2>Information We Collect</h2>
+        <p>
+          <b>Personal Information: </b>We collect information such as your name,
+          email address, and phone number when you voluntarily provide it
+          through our contact forms or other interactions on our website.
+        </p>
+        <p>
+          <b>Non-Personal Information: </b>We may collect non-personal
+          information such as your browser type, operating system, and Internet
+          service provider.
+        </p>
+        <p>
+          <b>Usage Data: </b>We collect information on how you use our website,
+          including your IP address, browser type, pages visited, and the time
+          and date of your visit.
+        </p>
+
+        <h2>How We Use Your Information</h2>
+        <ul>
+          <li>
+            <b>To Provide Services: </b>We use your information to provide and
+            maintain our services, including customer support.
+          </li>
+          <li>
+            <b>To Improve Our Website: </b>Your feedback helps us improve our
+            website and services.
+          </li>
+          <li>
+            <b>To Communicate: </b> We may use your contact information to send
+            you updates, newsletters, and marketing materials.
+          </li>
+        </ul>
+
+        <h2>How We Protect Your Information</h2>
+        <p>
+          We implement security measures to protect your personal information
+          from unauthorized access, alteration, disclosure, or destruction.
+        </p>
+        <h2>Sharing Your Information</h2>
+        <p>
+          We do not sell, trade, or rent your personal information to others. We
+          may share generic aggregated demographic information not linked to any
+          personal identification information with our business partners and
+          trusted affiliates.
+        </p>
+        <h2>Your Consent</h2>
+        <p>By using our site, you consent to our website privacy policy.</p>
+        <h2>Changes to This Privacy Policy</h2>
+        <p>
+          We may update this Privacy Policy from time to time. We will notify
+          you of any changes by posting the new Privacy Policy on our website.
+          You are advised to review this Privacy Policy periodically for any
+          changes.
+        </p>
+        <h2>Contact Us</h2>
+        <p>
+          If you have any questions about these Terms, please contact us at:
+        </p>
+        <ul>
+          <li>HealthWebExperts.com</li>
+          <li>
+            <b>Email:</b> info@healthwebexperts.com
+          </li>
+          <li>
+            <b>Phone: </b>(786) 855-4544
+          </li>
+        </ul>
+      </article>
+    </main>
+  );
 }
--- src/app/(pages)/terms-of-service/page.tsx
+++ src/app/(pages)/terms-of-service/page.tsx
@@ -1,5 +1,148 @@
 import React from "react";
 
 export default function Page() {
-  return <main>page</main>;
+  return (
+    <main>
+      <article className="article">
+        <h1>Terms of Service</h1>
+        <h4>Introduction</h4>
+        <p>
+          Welcome to HealthSiteSolutions.com. These terms of service ("Terms")
+          govern your use of our website and services. By accessing or using our
+          services, you agree to be bound by these Terms. If you do not agree
+          with any part of these Terms, please do not use our services.
+        </p>
+
+        <h3>Services</h3>
+        <p>
+          HealthSiteSolutions.com provides web development and design services
+          specifically for healthcare providers. Our services include but are
+          not limited to website design, development, SEO, content creation,
+          hosting, and maintenance.
+        </p>
+
+        <h2>User Responsibilities</h2>
+        <ul>
+          <li>
+            <b>Accurate Information: </b>You agree to provide accurate and
+            complete information when engaging our services.
+          </li>
+          <li>
+            <b>Compliance:</b> You agree to comply with all applicable laws and
+            regulations in connection with your use of our services.
+          </li>
+          <li>
+            <b>Security: </b>You are responsible for maintaining the
+            confidentiality of your account information and for all activities
+            that occur under your account.
+          </li>
+        </ul>
+        <h2>Intellectual Property</h2>
+        <ul>
+          <li>
+            <b>Ownership:</b>All content and materials on
+            HealthSiteSolutions.com, including text, graphics, logos, and
+            software, are the property of HealthSiteSolutions.com or its content
+            suppliers and are protected by intellectual property laws.
+          </li>
+          <li>
+            <b>License:</b>We grant you a limited, non-exclusive,
+            non-transferable, and revocable license to use our services for
+            their intended purpose. You may not reproduce, distribute, or create
+            derivative works from our content without our express written
+            permission.
+          </li>
+        </ul>
+        <h2>Payment Terms</h2>
+        <ul>
+          <li>
+            <b>Fees:</b> You agree to pay all fees specified for the chosen
+            service plan. Payment terms are outlined in the service agreement.
+          </li>
+          <li>
+            <b>Late Payments:</b> Late payments may incur additional charges or
+            result in suspension of services until the account is brought
+            current.
+          </li>
+          <li>
+            <b>Refunds: </b>Refund policies are outlined in the service
+            agreement. Generally, we do not offer refunds for completed
+            services.
+          </li>
+        </ul>
+        <h2>Confidentiality</h2>
+        <ul>
+          <li>
+            <b>Client Data: </b> We are committed to maintaining the
+            confidentiality of your data. We will not disclose your confidential
+            information to third parties without your consent, except as
+            required by law.
+          </li>
+          <li>
+            <b>Privacy Policy: </b>Our use of your personal information is
+            governed by our Privacy Policy, which is incorporated into these
+            Terms by reference.
+          </li>
+        </ul>
+        <h2>Termination</h2>
+        <ul>
+          <li>
+            <b>Termination by Client: </b>
+            You may terminate the service agreement at any time by providing
+            written notice. Termination fees may apply as outlined in the
+            service agreement.
+          </li>
+          <li>
+            <b>Termination by HealthSiteSolutions.com:</b>
+            We reserve the right to terminate or suspend services immediately,
+            without prior notice or liability, for any reason, including breach
+            of these Terms.
+          </li>
+        </ul>
+        <h2>Limitation of Liability</h2>
+        <ul>
+          <li>
+            <b>No Warranties:</b> Our services are provided "as is" and "as
+            available." We make no warranties, express or implied, regarding the
+            service.
+          </li>
+          <li>
+            <b>Limitation:</b> In no event shall HealthSiteSolutions.com be
+            liable for any indirect, incidental, special, consequential, or
+            punitive damages, or any loss of profits or revenues.
+          </li>
+        </ul>
+        <h2>Changes to Terms</h2>
+        <p>
+          We reserve the right to modify these Terms at any time. We will notify
+          you of any changes by posting the new Terms on our website. Your
+          continued use of the services after such changes constitutes your
+          acceptance of the new Terms.
+        </p>
+        <h2>Governing Law</h2>
+        <p>
+          These Terms shall be governed and construed in accordance with the
+          laws of [Your State/Country], without regard to its conflict of law
+          provisions.
+        </p>
+        <h2>Contact Us</h2>
+        <p>
+          If you have any questions about these Terms, please contact us at:
+        </p>
+        <ul>
+          <li>HealthWebExperts.com</li>
+          <li>
+            <b>Email:</b> info@healthwebexperts.com
+          </li>
+          <li>
+            <b>Phone: </b>(786) 855-4544
+          </li>
+        </ul>
+        <p>
+          By using our services, you acknowledge that you have read, understood,
+          and agree to be bound by these Terms of Service.
+        </p>
+      </article>
+    </main>
+  );
 }
