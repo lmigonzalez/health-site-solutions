418fb1801475a3893ed0db99e19a567f35e0aa5b
lmigonzalez <lmigonzalezs@gmail.com>
2024-08-26 18:30:02 -0400

    new industry

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,14 +4,14 @@ import HeroSocials from "@/components/HeroSocials";
 export default function Hero() {
   return (
     <section className="bg-my_blue relative flex min-h-[600px] w-full items-center justify-center bg-[url('/health-site-solutions-hero.webp')] bg-cover bg-left-bottom bg-no-repeat px-6 md:justify-end md:pt-0">
+      {/* <div className="absolute left-0 top-0 h-full w-full bg-gradient-to-r from-sky-300" /> */}
       <div className="flex h-fit flex-col items-center justify-center gap-6 rounded border bg-white bg-opacity-50 p-4 text-center backdrop-blur-sm md:w-1/2 md:items-end md:border-none md:bg-transparent md:text-end">
         <h1 className="text-2xl font-bold md:text-4xl">
           Expert Healthcare Web Design and Development Services
         </h1>
         <h2 className="md:text-lg">
-        Professional Medical Websites: Custom{" "}
-          <b>Hand-Coded Sites</b> Starting at $750 — No Page Builders or
-          WordPress
+          Professional Medical Websites: Custom <b>Hand-Coded Sites</b> Starting
+          at $750 — No Page Builders or WordPress
         </h2>
         <div className="flex justify-end pt-2">
           <Link
@@ -36,7 +36,7 @@ export default function Hero() {
           </Link>
         </div>
       </div>
-      <HeroSocials />
+      {/* <HeroSocials /> */}
     </section>
   );
 }
